fad4f5466a12e20c225ab5bd773aaf92c7bea380
RobertoGil <roberto.gil021@comunidadunir.net>
2021-07-18 09:18:24 +0000

    conf final

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # añadir tantas líneas como sean necesarias para el correcto despligue
-ansible-playbook -i hosts -l XXXX playbook
+ansible-playbook -i hosts playbook-kubernetes.yml
+ansible-playbook -i hosts playbook-app.yml
 
